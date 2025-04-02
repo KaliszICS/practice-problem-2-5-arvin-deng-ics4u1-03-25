8b2b22183e2a01d15802f92b60b0b705769eebc2
arvin-deng-ics4u1-03-25 <arvin.deng@student.tdsb.on.ca>
2025-04-02 18:10:06 -0400

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,15 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
+    public static void insertionSort(char[] characters){
+        for (int i = 1; i < characters.length; i++){
+            char key = characters[i];
+            int index = i - 1;
+            while (index >= 0 && key < characters[index]){
+                characters[index + 1] = characters[index];
+                index--;
+            }
+        characters [index + 1] = key;
+        }
+    }
 
 }
